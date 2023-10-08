c81bf3cf829957f198d5dfe5e8a6acfe979f21b8
Anna Gleń <anna.glen.archi@gmail.com>
2023-10-08 22:11:41 +0200

    playersChoice and computerChoice definition

---
+++ rockpaper.js
@@ -0,0 +1,29 @@
+function getComputerChoice() {
+    const randomNumberChoice = Math.random();
+    if (randomNumberChoice < 0.33) {
+        return "rock"
+    }
+    else if (randomNumberChoice >= 0.33 && randomNumberChoice <= 0.66) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function playRound(playersChoice, getComputerChoice) {
+    if (playersChoice == "rock" && getComputerChoice == "paper")
+        return "You loose"
+    else if (playersChoice == "paper" && getComputerChoice == "scissors")
+        return "You loose"
+    else if (playersChoice == "scissors" && getComputerChoice == "rock")
+        return "You loose"
+    else 
+        return "You win!"
+}
+
+const playerSelection = prompt("Rock, paper, scissors. Which one you choose?");
+const playersChoice = playerSelection.toLowerCase();
+
+
+
